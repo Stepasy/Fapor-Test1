db9c1ba16640148f9623573ce87c02b830c646e0
stepasy <sad100500@gmail.com>
2018-11-21 12:09:40 +0200

    respnsive footer

--- src/js/modules/start.js
+++ src/js/modules/start.js
@@ -10,6 +10,26 @@ $(document).ready(function() {
     });
     $('.owl-carousel').owlCarousel({
         items: 1,
-        loop: true
+        loop: true,
+        nav:true,
+        navText: [
+            '<i class="fas fa-angle-left fa-2x"></i>',
+            '<i class="fas fa-angle-right fa-2x"></i>'
+        ],
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:1,
+                nav:false
+            },
+            1000:{
+                items:1,
+                nav:true,
+            }
+        }
     });
 });
