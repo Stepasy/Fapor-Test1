a4cf9e93e338aff4e2e16c56e3883623d067225e
stepasy <sad100500@gmail.com>
2018-11-22 19:03:06 +0200

    responsive fixes

--- src/js/modules/owl.js
+++ src/js/modules/owl.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
+        margin: 0,
         items: 1,
         loop: true,
         nav:true,
@@ -7,20 +8,5 @@ $(document).ready(function(){
             '<i class="fas fa-angle-left fa-2x"></i>',
             '<i class="fas fa-angle-right fa-2x"></i>'
         ],
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-                nav:true
-            },
-            600:{
-                items:1,
-                nav:false
-            },
-            1000:{
-                items:1,
-                nav:true,
-            }
-        }
     });
 });
