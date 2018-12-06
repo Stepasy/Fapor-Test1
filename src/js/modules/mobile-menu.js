ade5428c70617544b907b8b235aca7707b5df524
stepasy <sad100500@gmail.com>
2018-12-06 12:17:43 +0200

    minor mobile menu js update

--- src/js/modules/mobile-menu.js
+++ src/js/modules/mobile-menu.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
     $(".fa-bars").click(function() {
         $(".navbar-holder").slideToggle();
     });
+    $(".navbar").click(function(){
+        $(".navbar-holder").slideToggle();
+    });
 });
