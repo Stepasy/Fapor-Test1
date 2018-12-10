8c5db3c9ef3a9b972eab23845e38c2829ebeadff
stepasy <sad100500@gmail.com>
2018-12-10 16:07:18 +0200

    all fixes after the last review

--- src/js/modules/slick.js
+++ src/js/modules/slick.js
@@ -3,8 +3,19 @@ $(document).ready(function() {
         slidesToShow: 1,
         slidesToScroll: 1,
         infinite: true,
-        responsive: true,
         focusOnSelect: false,
         accessibility: false,
+        nextArrow: '<i class="fa fa-chevron-right"></i>',
+        prevArrow: '<i class="fa fa-chevron-left"></i>',
+        responsive: [
+            {
+                breakpoint: 767,
+                settings:{
+                dots: true,
+                arrows: false,
+                }
+            }
+
+          ]
     });
 });
