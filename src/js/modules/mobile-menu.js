08bfb499851a6996cfe0492f40331e2e3501461e
stepasy <sad100500@gmail.com>
2018-12-13 09:29:34 +0200

    fixes after review

--- src/js/modules/mobile-menu.js
+++ src/js/modules/mobile-menu.js
@@ -2,7 +2,4 @@ $(document).ready(function() {
     $(".fa-bars").click(function() {
         $(".navbar-holder").slideToggle();
     });
-    $(".navbar").click(function(){
-        $(".navbar-holder").slideToggle();
-    });
 });
